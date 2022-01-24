60db2eca134603ce052d5331777fa630293e091d
garyarzumanyan <garyarzumanyan@gmail.com>
2022-01-23 23:56:37 -0800

    Added mongoDB functionality for get all service

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Phonebook = require('./models/phonebook')
 
 app.use(cors())
 app.use(express.json())
@@ -35,7 +37,7 @@ let persons = [
 ]
   
   app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Phonebook.find({}).then(persons => response.json(persons))
   })
 
   app.get('/info',(request, response) => {
@@ -91,7 +93,7 @@ let persons = [
 
  
 
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
---
+++ models/phonebook.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+    .then(() => {
+        return console.log("Successfuly Connected to MongoDB")
+    })
+    .catch(error => {
+        return console.log("Error connecting to MongDB!", error.message)
+    })
+    
+ const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+module.exports = mongoose.model('Phonebook', personSchema)
